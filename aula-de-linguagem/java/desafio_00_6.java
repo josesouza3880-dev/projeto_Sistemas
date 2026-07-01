3d8578279a0963688e235653710b1c8803332ae8
José Luiz <jose.souza3880@gmail.com>
2026-07-01 10:26:02 -0400

    aula_de_linguagem

---
+++ aula-de-linguagem/java/desafio_00_6.java
@@ -0,0 +1,10 @@
+public class soma_1 {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 3;
+        
+        int soma_1 = a + b;
+        System.out.println("A soma de "+ a + " e "
+         + b + " é: "+ soma_1 );
+    } 
+}
